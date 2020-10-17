81af3376ab6d19f1d7f07982c805fc3c826e4fac
medwardson <mnaidooe@uwaterloo.ca>
2020-10-17 14:31:38 -0400

    Format main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import sqlite3
 import add_student
 
-
 while __name__ == "__main__":
     print("Welcome to the TimeTable Program.\n Please select from our following options:")
     choice = input(">> Add new student (1)\n>> Quit (2)\n>> ")
